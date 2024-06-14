502f66105333954b31d415924ffd8d5636a7685b
Ivan-Feofanov <Ivan-Feofanov@users.noreply.github.com>
2024-06-14 15:14:48 +0000

    Add compiled typescript files

---
+++ consts/mainnet/core.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WSTETH_ADDRESS = exports.LIDO_STETH_ADDRESS = exports.LDO_ADDRESS = void 0;
+exports.LDO_ADDRESS = '0x5a98fcbea516cf06857215779fd812ca3bef1b32';
+exports.LIDO_STETH_ADDRESS = '0xae7ab96520de3a18e5e111b5eaab095312d7fe84';
+exports.WSTETH_ADDRESS = '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0';
