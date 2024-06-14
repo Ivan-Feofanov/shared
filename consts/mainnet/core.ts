96c352e5a876cbe8d34971c3c8d584fb4adb887b
Ivan Feofanov <feofanov@gmail.com>
2024-06-14 17:14:21 +0200

    Initial commit

---
+++ consts/mainnet/core.ts
@@ -0,0 +1,3 @@
+export const LDO_ADDRESS: string = '0x5a98fcbea516cf06857215779fd812ca3bef1b32'
+export const LIDO_STETH_ADDRESS: string = '0xae7ab96520de3a18e5e111b5eaab095312d7fe84'
+export const WSTETH_ADDRESS: string = '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0'
